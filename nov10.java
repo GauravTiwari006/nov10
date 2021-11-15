1e649b800833a5d0e0716b227d7fcb475f189214
Gaurav Tiwari <87387424+GauravTiwari006@users.noreply.github.com>
2021-11-15 15:09:37 +0530

    Add files via upload

---
+++ nov10.java
@@ -0,0 +1,23 @@
+
+public class nov10 {
+
+	public static void main(String[] args) {
+		
+		//  float value : 
+		
+		float r = 1004.115478f;
+		System.out.println("**********The float value is :******** " +  r); 
+		
+		//  double value : 
+		
+		double d = 14567.1458675267;
+		System.out.println("******double value is :***** " + d);
+		
+		// how to write character value : 
+		
+		char g = 'G';
+		System.out.println("******** character value is : ******" +g );
+		
+	}
+
+}
